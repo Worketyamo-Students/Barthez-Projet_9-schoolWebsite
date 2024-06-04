f9be0fcf8246cbec9cbac4dc012263aae268ef8e
Your Name <you@example.com>
2024-06-04 21:45:29 +0100

    premiere sauvegarde

---
+++ tailwind.js
@@ -0,0 +1,68 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                primaryColor: '#252B42',
+                primaryColor2: '#26335D',
+                primaryColor3: '#2435A1',
+                secondaryColor: '#FF6551',
+                secondaryColor2: '#E74040',
+                secondaryColor3: '#E77C40',
+                grayColor: '#737373',
+                grayColor2: '#E6E6E6',
+                grayColor3: '#BDBDBD',
+                grayColor4: '#F9F9F9',
+                greenColor: '#2DC071',
+                lightBlueColor: '#8EC2F2',
+            },
+
+            fontFamily: {
+                'body': ["Montserrat", "sans-serif"],
+            },
+    
+            fontSize: {
+                '4.5xl': '40px',
+                '5.5xl': '50px',
+                '6xl': '58px',
+            },
+            
+            boxShadow: {
+                'card': '0px 13px 19px 0px #00000012',
+                'sm-card': '0px 2px 4px 0px #0000001A',
+            },
+    
+            padding: {
+                '3.75': '15px',
+                '4.5': '19px',
+                '5.5': '22px',
+                '7.5': '30px',
+                '10.5': '42px',
+                '13': '50px',
+                '33': "132px",
+                '38': "150px",
+                '45': "195px",
+            },
+
+            gap: {
+                '8': '30px',
+            },
+
+            width: {
+                '13': '50px',
+                '25': '94px',
+            },
+
+            borderWidth: {
+                '1.5' : '1.5px',
+                '5' : '5px',
+                '7' : '7px',
+            },
+        }, 
+
+
+    }, 
+
+    plugins: [],
+  }
+
